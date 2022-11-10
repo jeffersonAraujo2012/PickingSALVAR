1a0f4dad06f06906292d8a013c7cf6404c5771d8
Jefferson Araújo <jeffersonc201212@gmail.com>
2022-11-10 00:16:45 -0300

    Add: Interface de entrada das mensagens de registro.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app  = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('Servidor online');
+});
